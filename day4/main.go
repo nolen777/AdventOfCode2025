2ab1c46666b85add41faf9ae1c994380639ad3e2
Dan Crosby <dan@danielcrosby.net>
2025-12-03 21:18:37 -0800

    part 1

---
+++ day4/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func readMap(filename string) [][]bool {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var m [][]bool
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		mapLine := make([]bool, len(line))
+		for idx, c := range line {
+			if c == '@' {
+				mapLine[idx] = true
+			}
+		}
+		m = append(m, mapLine)
+	}
+
+	return m
+}
+
+func validPosition(m [][]bool, r int, c int) bool {
+	if r < 0 {
+		return false
+	}
+	if c < 0 {
+		return false
+	}
+	if r >= len(m) {
+		return false
+	}
+	if c >= len(m[r]) {
+		return false
+	}
+	return true
+}
+
+func adjacentCount(m [][]bool, r int, c int) int {
+	count := 0
+	for i := -1; i <= 1; i++ {
+		if r+i < 0 {
+			continue
+		}
+		if r+i >= len(m) {
+			continue
+		}
+		for j := -1; j <= 1; j++ {
+			if i == 0 && j == 0 {
+				continue
+			}
+			if c+j < 0 {
+				continue
+			}
+			if c+j >= len(m[r+i]) {
+				continue
+			}
+			if m[r+i][c+j] {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func part1(m [][]bool) {
+	count := 0
+	for i := 0; i < len(m); i++ {
+		for j := 0; j < len(m[i]); j++ {
+			if !m[i][j] {
+				fmt.Print(".")
+				continue
+			}
+			adjCount := adjacentCount(m, i, j)
+
+			if adjCount < 4 {
+				count++
+				fmt.Print("x")
+			} else {
+				fmt.Print("@")
+			}
+		}
+		fmt.Println("")
+	}
+
+	fmt.Println("Total: ", count)
+}
+
+func part2(m [][]bool) {
+}
+
+func main() {
+	path := "day4/input.txt"
+	m := readMap(path)
+
+	fmt.Println("Part 1:")
+	part1(m)
+
+	fmt.Println("Part 2:")
+	part2(m)
+}
